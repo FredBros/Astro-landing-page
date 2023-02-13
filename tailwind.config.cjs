b8c5e748ad5427a1b8a37c4115cd151217c8953a
Fred Brossard <fred.b.devweb@gmail.com>
2023-02-13 12:57:00 +0100

    !rst commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: { Montserrat: ["Montserrat", "sans-serif"] },
+    },
+  },
+  plugins: [require("daisyui")],
+};
